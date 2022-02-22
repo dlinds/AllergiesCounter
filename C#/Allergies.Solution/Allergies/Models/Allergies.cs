eb2d8ff1bbb473181f26b14eb6803919ada86b3d
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-22 14:28:45 -0800

    pass first test
    Co-authored-by: Chase-Stokes <Chase-Stokes@users.noreply.github.com>
    Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

---
+++ C#/Allergies.Solution/Allergies/Models/Allergies.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Allergies
+{
+  public class AllergyChecker
+  {
+    public string AllergyCheck(int score)
+    {
+      return "eggs";
+    }
+  }
+}
